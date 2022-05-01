a51d8e3d3853280e6ab2fc37ee9a8d6089269806
Jodesen <62249320+Jodesen@users.noreply.github.com>
2022-05-01 14:57:09 +0800

    Delete u3 directory

--- u3/工作日的力量.py
+++
@@ -1,9 +0,0 @@
-#daydayup
-dayup = 1.0
-upbian =0.01
-for i in range(365):
-    if i % 7 in[6,0]:
-        dayup=dayup*(1-upbian)
-    else:
-        dayup=dayup*(1+upbian)
-print('工作日的力量是:{:.2f}'.format(dayup))
--- u3/星期转换.py
+++
@@ -1,4 +0,0 @@
-# 星期转换
-week ='一二三四五六日'
-weekid=eval(input('请输入星期数字:'))
-print('星期'+week[weekid-1])
